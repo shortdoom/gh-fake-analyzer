b9ff4a4ce8f808d012b437e76f2a8b99e3ac7027
shortdoom <oskar.milosz.wojciechowski@gmail.com>
2024-10-23 23:37:43 +0200

    feat: issues fetching

--- src/gh_fake_analyzer/terminal.py
+++ src/gh_fake_analyzer/terminal.py
@@ -7,6 +7,7 @@ from .modules.monitor import GitHubMonitor
 from .utils.api import APIUtils
 from .utils.config import setup_logging
 
+
 def read_targets(file_path):
     """Reads a list of GitHub usernames from a file."""
     try:
@@ -36,7 +37,9 @@ def process_target(username, commit_search=False, only_profile=False, out_path=N
                 logging.info(f"Profile data exists. Running filter commit search.")
                 analyzer.filter_commit_search()
             else:
-                logging.info(f"Profile data not found. Running analysis before commit search.")
+                logging.info(
+                    f"Profile data not found. Running analysis before commit search."
+                )
                 analyzer.run_analysis()
                 logging.info(f"Analysis done. Running filter commit search.")
                 analyzer.filter_commit_search()
@@ -53,32 +56,33 @@ def process_target(username, commit_search=False, only_profile=False, out_path=N
             logging.info(f"Processing completed for {username}")
     except Exception as e:
         logging.error(f"Error processing target {username}: {e}")
-        print(f"Error processing target {username}: {e}")
-        
+
 
 def terminal():
-    parser = argparse.ArgumentParser(description="Analyze GitHub profiles.")
+    parser = argparse.ArgumentParser(
+        description="Dump and analyze GitHub profiles. Focused on detecting fake developers, phishing, bot-networks and scammers."
+    )
     parser.add_argument(
         "username",
         type=str,
         nargs="?",
-        help="GitHub username to analyze (if omitted, reads from targets file)",
-    )
-    parser.add_argument(
-        "--monitor",
-        action="store_true",
-        help="Activate monitoring (event watcher) for the target or list of targets"
-    )
-    parser.add_argument(
-        "--only_profile",
-        action="store_true",
-        help="Only fetch profile data (no commits, followers, etc.)",
+        help="GitHub username to analyze",
     )
     parser.add_argument(
         "--targets",
         nargs="?",
         const="targets",
         help="File containing a list of GitHub usernames to analyze (defaults to 'targets')",
+    )
+    parser.add_argument(
+        "--monitor",
+        action="store_true",
+        help="Activate monitoring (event watcher) for the target or list of targets",
+    )
+    parser.add_argument(
+        "--only_profile",
+        action="store_true",
+        help="Only fetch profile data (no commits, followers, etc.)",
     )
     parser.add_argument(
         "--commit_search",
@@ -105,59 +109,48 @@ def terminal():
         logging.info("Using Github Token, higher rate limits apply")
     else:
         logging.warning("No GitHub token provided. Rate limits may apply.")
-        print("No GitHub token provided. Rate limits may apply.")
-        
-    # NOTE: Only start monitoring.log and listen for new events
-    # NOTE: After monitor() exits, exit terminal too
+
     if args.monitor:
         monitor = GitHubMonitor(APIUtils)
-        
+
         if args.username:
             monitor.monitor([args.username])
-        
+
         if args.targets:
             targets = read_targets(args.targets)
             if not targets:
                 logging.error(f"No targets found in {targets_file}. Exiting.")
-                print(
-                    f"No targets found in {targets_file}. Please provide a valid targets file or specify a username."
-                )
                 return
             monitor.monitor(targets)
-        
+
     if args.only_profile:
         logging.info(f"Only fetching profile data for {args.username}...")
         process_target(args.username, only_profile=True, out_path=args.out_path)
         return
 
-    if args.username:        
+    if args.username:
         logging.info(f"Processing single target: {args.username}")
         process_target(args.username, args.commit_search, out_path=args.out_path)
 
     if args.targets:
         targets_file = args.targets
         logging.info(f"Processing targets from file: {targets_file}")
-        print(f"Processing targets from file: {targets_file}")
 
         targets = read_targets(targets_file)
         if not targets:
             logging.error(f"No targets found in {targets_file}. Exiting.")
-            print(
-                f"No targets found in {targets_file}. Please provide a valid targets file or specify a username."
-            )
             return
 
         for target in targets:
             logging.info(f"Processing target: {target}")
             process_target(target, args.commit_search, out_path=args.out_path)
-    
+
     if not args.username and not args.targets:
         logging.error("No targets specified. Exiting.")
-        print("No targets specified. Please provide a valid username or targets file.")
-        print("Print help with -h or --help.")
+        logging.info("No targets specified. Please provide a valid username or targets file.")
+        logging.info("Print help with -h or --help.")
 
     end_time = time.time()
-    print(f"Processing completed in {end_time - start_time:.2f} seconds.")
     logging.info(f"Processing completed in {end_time - start_time:.2f} seconds.")
 
 
